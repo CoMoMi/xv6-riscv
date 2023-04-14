05161f2c7942369921dd32b62d04d7d4f5a84d10
dooh1 <dooh1@unlv.nevada.edu>
2023-04-14 01:21:03 -0700

    finalized schtest and changed 1cpu to 3

--- user/schtest.c
+++ user/schtest.c
@@ -10,6 +10,7 @@
 // "Process 0 started (priority 0x0A)"
 // "Process 0 finished (priority 0x0A)"
 
+
 void do_work(int processNum, int priority){
     int i, j;
     long long int count = 0;
@@ -24,40 +25,28 @@ void do_work(int processNum, int priority){
 int main(int argc, char *argv[])
 {
     int pid;
-    //int status;
     
     int children[10] = {0x0A, 0x0D, 0x0C, 0x0A, 0x0F, 0x0A, 0x0A, 0x0F, 0x0A, 0x0C};
-    // fork 10 children
+    // fork 10 children with different priorities
     for (int i = 0; i < 10; i++) {
         pid = fork();
         
-        if (pid < 0)
-        {
+        if (pid < 0){
             printf("Error: fork failed\n");
             exit(1);
         }
-        
         // child
         else if (pid == 0) {
 
-            // if(setpri(children[i]) != 0) {
-            //     printf("Error: setpri failed\n");
-            //     exit(1);
-            // }
             setpri(children[i]);
-            sleep(5);
+            sleep(1);
             do_work(i, children[i]);
-
-            //printf("Process %d finished (priority %d)\n", i, getpri());
             exit(0);
         }
         else if (pid > 0) {
             // parent
-            
             printf("Process %d started (priority 0x%x)\n", i, children[i]);
         }
-
-
     }
     // wait for all child processes to finish
     for(int i=0; i < 10; i++) {
